fdc756216c981dda0489d52308253a8950c4ae1f
huhp <huhp@tingyun.com>
2019-06-27 08:16:57 +0800

    添加上基本的嵌码的逻辑的信息

--- lib/tabs/network.dart
+++ lib/tabs/network.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class Network extends StatelessWidget{
+
+class Network extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
@@ -14,11 +16,26 @@ class Network extends StatelessWidget{
               size: 160.0,
               color: Colors.red,
             ),
-            new Text('First Tab')
+            new Text('First Tab'),
+            RaisedButton(
+              onPressed: () {
+                print("clicked");
+                loadData();
+              },
+              child: Text("button click"),
+            ),
           ],
         ),
       ),
     );
   }
 
+  loadData() async{
+    String dataURL = "http://www.qq.com";
+
+    http.Response response = await http.get(dataURL);
+    
+    print('result' + response.body);
+
+  }
 }
